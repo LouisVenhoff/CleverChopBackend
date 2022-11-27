39e3522a6e01bb33c0bb932cf0807e50854ba45a
DESKTOP-OH4MRFL\Louis Venhoff <Louis.venhoff@gmx.net>
2022-11-27 17:11:40 +0100

    Added ean api fetching

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var eanApiController_1 = require("./classes/eanApiController");
 var express = require("express");
 var app = express();
 var port = 3014;
+var eanSource = new eanApiController_1["default"]("400000000");
 app.get("/", function (req, res) {
     console.log("Working");
 });
 app.get("/api/sendCode/:code", function (req, res) {
     console.log("Got code Route" + req.params.code);
+    eanSource.requestEan(req.params.code);
 });
 app.listen(port, function () {
     console.log("Listening on Port " + port);
--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+import EanApiController from "./classes/eanApiController";
+
 const express = require("express");
 
 const app = express();
 const port = 3014;
+
+const eanSource:EanApiController = new EanApiController("400000000");
 
 
 app.get("/", (req:any, res:any) => {
@@ -10,6 +14,8 @@ app.get("/", (req:any, res:any) => {
 
 app.get("/api/sendCode/:code", (req:any, res:any) => {
     console.log("Got code Route" + req.params.code);
+
+    eanSource.requestEan(req.params.code);
 })
 
 app.listen(port, () => {
