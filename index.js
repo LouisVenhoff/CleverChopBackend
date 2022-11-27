2cbd16b4c3b1b3ac1e5427a0a982ef2b31d7d102
DESKTOP-OH4MRFL\Louis Venhoff <Louis.venhoff@gmx.net>
2022-11-27 15:02:13 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+app.get("/", function (req, res) {
+    console.log("Working");
+});
+app.get("/api/sendCode/:code", function (req, res) {
+    console.log("Got code Route" + req.params.code);
+});
+app.listen(port, function () {
+    console.log("Listening on Port " + port);
+});
---
+++ index.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req:any, res:any) => {
+    console.log("Working");
+})
+
+app.get("/api/sendCode/:code", (req:any, res:any) => {
+    console.log("Got code Route" + req.params.code);
+    
+})
+
+
+
+app.listen(port, () => {
+    console.log("Listening on Port " + port);
+})
